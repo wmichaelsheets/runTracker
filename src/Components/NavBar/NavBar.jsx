c810a7989ed4a63f34a57586c85ae7ebf7a1ebdc
Mike Sheets <wmichaelsheets@gmx.com>
2024-10-11 00:37:40 -0400

    Added drop-downs to NavBar. Linked to Register page.

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,16 +5,24 @@ export const NavBar = () => {
   return (
     <div className="nav-bar-container">
       <ul className="nav-bar">
-        <li>
-          <Link to={'/runs'}>Runs</Link>
+        <li className="dropdown">
+          <span className="dropbtn">Runs</span>
+          <div className="dropdown-content">
+            <Link to="/runs/enter">Enter a Run</Link>
+            <Link to="/runs/by-shoe">View Runs by Shoe</Link>
+          </div>
+        </li>
+        <li className="dropdown">
+          <span className="dropbtn">Shoes</span>
+          <div className="dropdown-content">
+            <Link to="/shoes/enter">Enter a Shoe</Link>
+            <Link to="/shoes/all">View all Shoes</Link>
+          </div>
         </li>
         <li>
-          <Link to={'/shoes'}>Shoes</Link>
-        </li>
-        <li>
-          <Link to={'/users'}>Users</Link>
+          <Link to="/register">Users</Link>
         </li>
       </ul>
     </div>
-  );
-};
+  )
+}
